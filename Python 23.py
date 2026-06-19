3c2840ef09058c611f70aa0b35443610c6d28e77
suyashapawar-web <suyashapawar@gmail.com>
2026-06-19 11:03:04 +0100

    Add files via upload

---
+++ Python 23.py
@@ -0,0 +1,12 @@
+class student:
+    grade = 10
+    name = "Penguin"
+    def introduction(self):
+        print("HI I am a student")
+    def details(self):
+        print("My name is", self.name)
+        print("I study in grade", self.grade)
+
+ob = student()
+ob.introduction()
+ob.details()
