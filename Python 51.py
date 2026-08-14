310d4f0d4c65ec24f757baeb0816aa4a8d3014d1
suyashapawar-web <suyashapawar@gmail.com>
2026-08-14 12:53:59 +0100

    Add files via upload

---
+++ Python 51.py
@@ -0,0 +1,7 @@
+import math
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# math.lcm() handles everything automatically
+print("LCM is:", math.lcm(num1, num2))
